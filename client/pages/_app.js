c07d44df5e73875892db78b36ead7911e2921da7
Mohamed <mkaouane1@gmail.com>
2021-12-16 05:43:31 +0100

    setup server

---
+++ client/pages/_app.js
@@ -0,0 +1,11 @@
+import TopNav from '../components/TopNav'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <>
+  <TopNav/>
+  <Component {...pageProps} />
+  </>
+}
+
+export default MyApp
---
+++ client/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import TopNav from '../components/TopNav'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      
+    </>
+  )
+}
