e86df081b9fafae6fd149e7d46ec57deb4eb8394
Mohamed <mkaouane1@gmail.com>
2021-12-29 03:18:59 +0100

    responsive content

--- client/pages/lms-content.jsx
+++ client/pages/lms-content.jsx
@@ -3,11 +3,13 @@ import Lms from './lms'
 import AsideNav from '../components/AsideNav'
 import HeaderNav from '../components/HeaderNav'
 
+
 export default function LmsContent() {
+
   return (
     <>
-      <div>
-        <div class="h-screen w-full bg-white relative flex overflow-hidden">
+ 
+        <div class="h-screen w-full bg-white relative flex  overflow-y-scroll">
 
           {/* <!-- Sidebar --> */}
           <AsideNav />
@@ -19,8 +21,10 @@ export default function LmsContent() {
             {/* <!-- Main --> */}
             <main class="max-w-full h-full flex relative overflow-y-hidden">
               {/* <!-- Container --> */}
-              <div class="h-full w-full m-4 flex flex-wrap items-start justify-start rounded-tl grid-flow-col auto-cols-max gap-4 overflow-y-scroll">
+              <div class="h-full w-full m-4 pb-6 flex flex-wrap items-start justify-start rounded-tl grid-flow-col auto-cols-max gap-4 overflow-y-scroll">
                 {/* <!-- Container --> */}
+                <div class="w-60 lg:w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400"></div>
+                <div class="w-60 lg:w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400"></div>
                 <div class="w-60 lg:w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400"></div>
                 <div class="w-60 lg:w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400"></div>
                 <div class="w-60 lg:w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400"></div>
@@ -40,7 +44,7 @@ export default function LmsContent() {
           </div>
 
         </div>
-      </div>
+
 
     </>
   )
